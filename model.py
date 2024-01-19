b1e547c67ab9f535fee2ee2393657cdbc115178a
1Wesleyyou <yuchenyou76@gmail.com>
2024-01-19 16:50:14 +0100

    feat: define the unet model

---
+++ model.py
@@ -0,0 +1,42 @@
+import torch.nn as nn
+import torch
+
+
+class UnetModel(nn.Module):
+    def __int__(self):
+        super(UnetModel, self).__init__()
+
+        # 定义编码器（下采样部分，U的左半边）
+        self.encoder = nn.Sequential(
+            # CT 是灰度图, 输入维度是 1
+            nn.Conv2d(1, 64, kernel_size=3, padding=1),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(64, 64, kernel_size=3, padding=1),
+            nn.ReLU(inplace=True),
+            nn.MaxPool2d(kernel_size=2, stride=2)
+        )
+
+        # 定义中间连接
+        self.middle = nn.Sequential(
+            # padding 就是在图片周围加上一圈 0 防止边界特征被弱化
+            nn.Conv2d(64, 128, kernel_size=3, padding=1),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(128, 128, kernel_size=3, padding=1),
+            nn.ReLU(inplace=True),
+            nn.MaxPool2d(kernel_size=2, stride=2)
+        )
+
+        # 定义解码器（上采样部分）
+        self.decoder = nn.Sequential(
+            nn.Conv2d(128, 64, kernel_size=3, padding=1),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(64, 64, kernel_size=3, padding=1),
+            nn.ReLU(inplace=True),
+            nn.ConvTranspose2d(64, 1, kernel_size=2, stride=2)
+        )
+
+    def forward(self, x):
+        x = self.encoder(x)
+        x = self.middle(x)
+        x = self.decoder(x)
+        return x
